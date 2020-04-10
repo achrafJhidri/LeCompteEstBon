13c09bd9cf5706e5e6bf62e932a3bcb3f4f1761b
Jhidri Achraf <achrafjhidri@gmail.com>
2020-04-10 11:14:38 +0200

    controlleur

---
+++ controller/gameController.py
@@ -0,0 +1,17 @@
+from model.Game import Game
+from view.MenuPrincipal import MenuPrincipal
+from view.View import View
+
+
+class Controller :
+    def __init__(self):
+        self.game = Game()
+        self.vue=View(self)
+
+
+    def run(self):
+        self.vue.mainloop()
+
+    def setVue(self,view):
+
+        self.vue.frame=view
