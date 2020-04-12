4ef7f1e8ba7f35d0f75c01c592d5866460d86a42
Jhidri Achraf <achrafjhidri@gmail.com>
2020-04-12 11:37:17 +0200

    firsts view ugly

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from controller.gameController import Controller
+import os
+
+#
+# if os.path.isfile("assets/skarlette.jpeg"):
+#
+#      print("image exists")
+# else:
+#      print ("mage does not exits")
+
 ctrl = Controller()
 ctrl.run()
-
-#commentaire
--- view/View.py
+++ view/View.py
@@ -1,15 +1,20 @@
-
 import tkinter as tk
 
 from view.MenuPrincipal import MenuPrincipal
+from view.Pseudo import Pseudo
 
 
 class View(tk.Tk):
-    def __init__(self,controller):
+    def __init__(self, controller):
         tk.Tk.__init__(self)
-        self.frame = MenuPrincipal(controller,self)
+        self.frame = Pseudo(controller, self)
 
+        screen_height = int(self.winfo_screenheight() * 2 / 3)
+        screen_width = self.winfo_screenwidth()
 
-        self.geometry("500x500")
+        screen_width = int(screen_width * 2 / 3)
+        # print(screen_width)
 
-        self.controller=controller
+        self.geometry("{0}x{1}".format(screen_width, screen_height))
+
+        self.controller = controller
