f05ef5599db8acb6c4d99f6bea5282f6164cfbb3
Jhidri Achraf <achrafjhidri@gmail.com>
2020-04-12 17:34:09 +0200

    myButton

---
+++ view/MyButton.py
@@ -0,0 +1,20 @@
+from tkinter import  Button
+import tkinter as tk
+
+class MyButton(tk.Button):
+
+    def __init__(self,master,**options):
+        tk.Button.__init__(self,master=master)
+        self.config(options)
+        self.used=False
+
+    def use(self):
+        self.used=True
+        self.config(state="disabled")
+
+    def unUse(self):
+        self.used=False
+        self.config(state="disabled")
+
+    def isUsed(self):
+        return self.used
