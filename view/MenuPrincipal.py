972487349e44d571c03ddeca6c1c71e4f0867455
Jhidri Achraf <achrafjhidri@gmail.com>
2020-04-12 12:39:50 +0200

    initGameCards fixed

--- view/MenuPrincipal.py
+++ view/MenuPrincipal.py
@@ -5,35 +5,35 @@ from view.EntrainementVue import EntrainementVue
 
 
 class MenuPrincipal(tk.Frame):
-    def __init__(self,controller,master):
+    def __init__(self,controller,master,previous):
         tk.Frame.__init__(self,master=master)
-        self.config(width=200,height=200,bd=1,relief="sunken")
-
-        self.pack()
-
-
+        self.config()
         self.controller=controller
+        self.previous=previous
         self.createWidgets()
 
-        self.entrainementVue=None
+        self.entrainementVue =None
 
 
 
+    def show(self):
+        self.pack(expand="yes")
 
 
     def createWidgets(self):
+        self.back = tk.Button(self, text="go back", fg="blue", command=self.on_back)
+        self.back.pack(side="bottom",expand="yes")
 
         self.entrainement = tk.Button(self, text="entrainement", fg="blue",  command=self.on_entrainement)
-        self.entrainement.pack(expand="no")
+        self.entrainement.pack(side="left",padx=10,pady=20,expand="yes")
 
-
-        self.multiPlayer = tk.Button(self, text="1vs1", command=self.on_1vs1)
-        self.multiPlayer.pack(expand="no")
+        self.multiPlayer = tk.Button(self, text="multi-joueurs", command=self.on_1vs1)
+        self.multiPlayer.pack(side="left",padx=10,pady=20,expand="yes")
 
 
     def on_entrainement(self):
         self.controller.initTrainingGame()
-        if ( self.entrainementVue is None):
+        if self.entrainementVue is None :
             self.entrainementVue = EntrainementVue(controller=self.controller, previous=self, master=self.master)
         self.pack_forget()
         self.entrainementVue.pack()
@@ -43,7 +43,7 @@ class MenuPrincipal(tk.Frame):
         # self.controller.go1vs1()
         print("go etat 1vs1")
 
+    def on_back(self):
+        self.pack_forget()
+        self.previous.pack()
 
-# root = tk.Tk()
-# app = Application(master=root)
-# app.mainloop()
