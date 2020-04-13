48986f87a74f2865ab0103a716264c394a8b6729
Mohamed <moh.chamekh@gmail.com>
2020-04-13 14:24:17 +0200

    history

---
+++ model/History.py
@@ -0,0 +1,26 @@
+from model.expressions.binaryEx import BinaryExpression
+
+
+class History:
+
+    def __init__(self):
+        self.currentOperation = None
+        self.listOperations = list() #list of vectors (LeftOp, Op, RightOp)
+
+
+    def addOperande(self):
+        pass
+
+    def addOperator(self):
+        pass
+
+    def erase(self):
+        pass
+
+    def deleteLastOperation(self):
+        pass
+
+
+
+
+
