2d559343aae1f152f2ddafb857f2fdc2f67b3039
Jhidri Achraf <achrafjhidri@gmail.com>
2020-04-15 21:52:51 +0200

    solution is ready

--- model/Solution.py
+++ model/Solution.py
@@ -5,10 +5,30 @@ class Solution :
 
     def __init__(self):
         self.log=list()
-    def __call__(self,cards,target):
+        self.solutionIndex=0
+    def solve(self, cards ,target):
+        i = 0
+        entiers = list()
+        while i < len(cards) :
+            # on transforme les Cards en entiers
+            entiers.append(cards[i].value)
+            i+=1
+
         stop=list()
         stop.append(False)
-        algo(entiers=cards,aTrouver=target,log=self.log,stop=stop)
-        print(self.log)
-        return self.log
+        algo(entiers=entiers,aTrouver=target,log=self.log,stop=stop)
+
+        return len(self.log) > 0
+
+    def getSolution(self):
+        result = None
+        if self.solutionIndex < len(self.log) :
+            result=  self.log[self.solutionIndex]
+            self.solutionIndex+=1
+        return result
+
+
+
+    def resetSolution(self):
+        self.solutionIndex=0
 
--- model/TrainingGame.py
+++ model/TrainingGame.py
@@ -2,9 +2,7 @@
 from random import randrange
 
 from model.Game import Game
-from model.expressions.Ex import Constant
-from model.expressions.binaryEx import BinaryExpression
-from model.operators.binaryOperators import Minus, Divide
+from model.Solution import Solution
 
 
 class TrainingGame(Game):
@@ -30,54 +28,14 @@ class TrainingGame(Game):
     def load(self):
         valid = False
 
-        while not valid :
-            # making a copy of the list
-            # so that we can at any moment do a backUp
-            cardsCopy = self.cards.copy()
-            opIndex = randrange(0, 4)  # choosing one random operator between + * / -
-            left = self.popRandomCard(cardsCopy).value  # choosing one random operand between the 6 cards
-            right = self.popRandomCard(cardsCopy).value  # choosing one random operand between the 5 cards left
-            op = self.operators[opIndex]
-
-            if TrainingGame.validate(left,op,right):
-                leftC = Constant(left)
-                rightC = Constant(right)
-
-                self.solution = BinaryExpression(leftC, op, rightC)  # constructing the first operation ( 10+5)
-                self.profondeur = 1  # setting the depth
-
-                # choosing the number of cards to use
-                rand = randrange(3, len(self.cards)) - 1
-
-                for i in range(0, rand):
-                    opIndex = randrange(0, 3)
-                    op = self.operators[opIndex]
-
-                    right = self.popRandomCard(cardsCopy).value
-
-                    rightC = Constant(right)
-
-                    self.solution = BinaryExpression(self.solution, op, rightC)#appending operations to the first operation (left side )
-
-                    # print(self.solution)
-                    self.profondeur += 1
-
-
-                value = self.solution.evaluate()
-
-                if Game.MIN <= value <= Game.MAX:
-                    # valid = True
-                    return value
-
-    def displaySolution(self):
-        depth = self.profondeur
-        expr = self.solution
-        while depth > self.solutionDepth+1:
-            expr = expr.left
-            depth -= 1
-
-        self.solutionDepth+=1
-        return expr
+    def solve(self):
+        self.solution = Solution()
+        solutionFound = self.solution.solve(self.cards, self.target)
+        while not solutionFound:
+            print("not found")
+            self.initCards()
+            self.setRandomNumber()
+            solutionFound = self.solution.solve(self.cards, self.target)
 
     def getSolution(self):
         if self.solutionDepth != self.profondeur:
@@ -86,5 +44,6 @@ class TrainingGame(Game):
             return None
 
 
-    def resetSolutionDepth(self):
-        self.solutionDepth=0
+    def init(self):
+        Game.init(self) #call the init of the parent
+        self.solve()
