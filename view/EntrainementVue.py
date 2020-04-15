f9dd1c216063f3e210fba3f21327197e8491acb9
Jhidri Achraf <achrafjhidri@gmail.com>
2020-04-15 22:03:26 +0200

    cleaning the code of entertainement view and use pack instead of grid

--- view/EntrainementVue.py
+++ view/EntrainementVue.py
@@ -11,42 +11,37 @@ class EntrainementVue(tk.Frame):
         tk.Frame.__init__(self,master=master)
         self.previous=previous
         self.controller=controller
-        self.title = tk.Frame(self,bd=1)
-        self.title.grid(row=0,column=0,columnspan=2)
-        tk.Label(self.title, text="ENTRAINEMENT", font=tkFont.Font(size="32")).pack()
-        self.bottomFrame = tk.Frame(self, bd=1)
-        self.bottomFrame.grid(row=2,column=0,columnspan=2)
+        self.initBottomFrame()
+        self.initTitleFrame()
         self.createWidgets()
+
         self.gameBoard = GameBoard(self, self.controller)
-        self.gameBoard.grid(row=1,column=0)
+        self.gameBoard.pack(side="left")
 
         self.solutionBoard = Solution(master=self,controller=self.controller)
-        self.solutionBoard.grid(row=1,column=1,padx=100,pady=100)
+        self.solutionBoard.pack(side="left")
 
+    def initBottomFrame(self):
+        self.bottomFrame = tk.Frame(self,background="red")
+        self.bottomFrame.pack(side="bottom",expand="yes",fill="both")
+
+    def initTitleFrame(self):
+        self.title = tk.Frame(self)
+        self.title.pack(side="top")
+        tk.Label(self.title, text="ENTRAINEMENT", font=tkFont.Font(size="32")).pack()
 
     def createWidgets(self):
         self.back = tk.Button(self.bottomFrame, text="BACK", fg="blue",  command=self.on_back,width=20)
-        self.back.grid(column=0 ,row = 0 )
+        self.back.grid(column=0 ,row = 0 ,padx=120)
         self.replay = tk.Button(self.bottomFrame, text="REPLAY", fg="blue", command=self.on_replay,width=20)
         self.replay.grid(column=1,row = 0)
 
     def onResetSolution(self):
         self.solutionBoard.onResetSolution()
-
-
-
     def on_back(self):
         self.forget()
         self.previous.pack(expand="yes")
-    def on_click(self):
-        print(self.back)
 
     def on_replay(self):
         self.gameBoard.replay()
         self.solutionBoard.onResetSolution()
-
-
-
-# root = tk.Tk()
-# app = Application(master=root)
-# app.mainloop()
